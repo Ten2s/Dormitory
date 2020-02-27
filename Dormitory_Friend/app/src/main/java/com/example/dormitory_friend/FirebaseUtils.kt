0c988658cde0d63b107856129bc202cb4611193a
Ten2s <45139267+Ten2s@users.noreply.github.com>
2020-02-27 18:08:07 +0900

    profile image

--- Dormitory_Friend/app/src/main/java/com/example/dormitory_friend/FirebaseUtils.kt
+++ Dormitory_Friend/app/src/main/java/com/example/dormitory_friend/FirebaseUtils.kt
@@ -5,6 +5,7 @@ import com.example.dormitory_friend.chat.chatData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 
 
 class FirebaseUtils
@@ -13,7 +14,7 @@ class FirebaseUtils
         var auth = FirebaseAuth.getInstance()
         val db = FirebaseFirestore.getInstance()
         val realtime = FirebaseDatabase.getInstance().reference.child("chatRoom")
-        var checkcnt = 1
+        val firestorage = FirebaseStorage.getInstance().reference
 
         fun getUid(): String {
             return auth.currentUser?.uid.toString()
--- Dormitory_Friend/app/src/main/java/com/example/dormitory_friend/firstfragments/MyFragment.kt
+++ Dormitory_Friend/app/src/main/java/com/example/dormitory_friend/firstfragments/MyFragment.kt
@@ -1,23 +1,27 @@
 package com.example.dormitory_friend.firstfragments
 
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.dormitory_friend.*
-
-import com.google.firebase.firestore.DocumentSnapshot
-import kotlinx.android.synthetic.main.activity_charicteristic.view.*
+import com.github.dhaval2404.imagepicker.ImagePicker
 import kotlinx.android.synthetic.main.fragment_my.*
 import kotlinx.android.synthetic.main.fragment_my.view.*
+import java.io.FileInputStream
 
 /**
  * A simple [Fragment] subclass.
  */
 class MyFragment : Fragment() {
+
+    val profileRef = FirebaseUtils.firestorage.child("profileImg").child(FirebaseUtils.getUid())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,11 +44,26 @@ class MyFragment : Fragment() {
                     }
             }
 
+
+
+        //change profile image
+        view.profile_img.setOnClickListener{
+            ImagePicker.with(this)
+                .galleryOnly()
+                .crop(1f, 1f)               //Crop Square image(Optional)
+                .compress(1024)         //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)  //Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
+
+
+        //modify info
         view.modify_txt.setOnClickListener {
             val intent = Intent(context, CharicteristicActivity::class.java)
             startActivity(intent)
         }
 
+        //log out
         view.logout_txt.setOnClickListener {
             FirebaseUtils.auth.signOut()
             val intent = Intent(context, MainActivity::class.java)
@@ -61,5 +80,22 @@ class MyFragment : Fragment() {
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val fileUri = data?.data
+            val file = ImagePicker.getFile(data)
+            val uploadTask = profileRef.putStream(FileInputStream(file))
+            uploadTask.addOnSuccessListener {
+                Glide.with(this).load(profileRef).into(this.profile_img)
+                Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+            }
 
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
